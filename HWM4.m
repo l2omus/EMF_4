59c450d84019749e6382c25cc344262cf307f6a2
Romain Pauli <romain.pauli@unil.ch>
2014-04-08 15:35:16 +0200

    Point 1a time-series regressions and cross-sectional regression

--- HWM4.m
+++ HWM4.m
@@ -1,4 +1,4 @@
-clear all; clc, close all; 
+close all, clear all; clc; 
 
 %--------------------------------------- Empirical Methods in Finance, Homework 3 -------------------------------------------------%
 %----------------------------------------------------------------------------------------------------------------------------------%
@@ -21,9 +21,55 @@ clear all; clc, close all;
 data_stocks=xlsread('DATA_HW3.xls','Stocks');
 data_market=xlsread('DATA_HW3.xls','Market');
 
-rmrf = data_market(:, 2)/100;
-smb = data_market(:,3)/100;
-hml = data_market(:,4)/100;
-rf = data_market(:,5)/100;
+rmrf = data_market(2:end, 2)/100;
+smb = data_market(2:end,3)/100;
+hml = data_market(2:end,4)/100;
+rf = data_market(2:end,5)/100;
 
 stocks_P = data_stocks(2:end,2:end);
+
+N = length(stocks_P(1,:));
+T = length(stocks_P(1:end-1,1));
+
+for i=1:N;                      %Create a matrix of returns for each firm and each period
+    for t = 1:T;
+        stocks_R(t, i) = (stocks_P(t+1,i) - stocks_P(t,i))/stocks_P(t, i);
+    end
+    stocks_Z(:,i) = stocks_R(:,i) - rf; %Create the same matrix for excess returns
+end
+
+%% Fama-French Factors
+
+%--------------------------------------- Section 1 -----------------------------------------------------
+%
+%
+%-------------------------------------------------------------------------------------------------------
+
+% Regression time-series 
+regressors_T = [ones(size(rmrf)) rmrf smb	hml];
+
+for i=1:N
+	reg_T(i) = ols(stocks_Z(:,i), regressors_T);
+	alphas(i,1) = reg_T(i).beta(1);
+    betas_m(i,1) = reg_T(i).beta(2);
+    betas_smb(i,1) = reg_T(i).beta(3);
+    betas_hml(i,1) = reg_T(i).beta(4);
+    residuals(:,i) = reg_T(i).resid;
+end
+
+% Regression cross-sectional
+
+stocks_mean_Z = mean(stocks_Z, 1)';
+
+regressors_N = [ones(size(alphas)) betas_m betas_smb betas_hml];
+
+reg_N = ols(stocks_mean_Z, regressors_N);
+prt(reg_N);
+
+
+
+
+
+
+
+
